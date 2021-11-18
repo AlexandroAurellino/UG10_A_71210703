4b4fdc65c276f85aa3b20f473e21cada6eb979a3
AlexandroAurellino <93307651+AlexandroAurellino@users.noreply.github.com>
2021-11-18 22:39:28 +0700

    Soal 4

---
+++ 4_A_71210703.py
@@ -0,0 +1,11 @@
+A = input("Masukkan artikel yang ingin dipindai: ")
+B = input("Masukkan nama klub favorit anda: ")
+C = input("Masukkan skor yang ingin dicari: ")
+if B in A and C in A:
+    print("Hasil pencarian ditemukan")
+elif B in A:
+    print("Hanya",B,"yang ditemukan pada artikel ini")
+elif C in A:
+    print("Hanya",C,"yang ditemukan pada artikel ini")
+else:
+    print("Hasil pencarian",B,"dan",C,"tidak ditemukan")

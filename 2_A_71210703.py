2362bfcecd3a6cd0a26142bb5ab9796b191cd7a6
AlexandroAurellino <93307651+AlexandroAurellino@users.noreply.github.com>
2021-11-18 21:20:19 +0700

    Soal 2

---
+++ 2_A_71210703.py
@@ -0,0 +1,24 @@
+print("############################")
+print("Kalkulator Advance Sederhana")
+print("############################")
+print("1. Menghitung sisa hasil bagai")
+print("2. Membulatkan ke bawah hasil pembagian")
+print("3. Mencari akar kubik sebuah bilangan")
+
+A = int(input("masukkan menu yang anda pilih: "))
+if A == 1:
+    x = int(input("Masukkan bilangan yang ingin dibagi: "))
+    y = int(input("Masukkan bilangan pembagi: "))
+    z = int(x%y)
+    print("Sisa hasil bagi",float(x),"dibagi dengan",float(y),"adalah",float(z))
+elif A==2:
+    x = int(input("Masukkan bilangan yang ingin dibagi: "))
+    y = int(input("Masukkan bilangan pembagi: "))
+    z = int(round(x/y,1))
+    print("Hasil pembagian",float(x),"dibagi dengan",float(y),"dibulatkan kebawah adalah",float(z))
+elif A==3:
+    x = int(input("Masukkan bilangan yang ingin dicari akar kubiknya "))
+    y = int(x**(1/3))
+    print("Akar kubik dari",float(x),"adalah",float(y))
+else:
+    print("Menu yang anda pilih tidak tersedia")
